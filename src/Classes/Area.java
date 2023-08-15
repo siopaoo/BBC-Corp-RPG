59d42cfcc9d3c2996f6f0ab4e17c6a9e4b26326e
siopaoo <96367815+siopaoo@users.noreply.github.com>
2023-08-15 10:19:54 +0800

    Update Area.java
    ouuu slayed

--- src/Classes/Area.java
+++ src/Classes/Area.java
@@ -13,5 +13,38 @@ public class Area {
     protected String name, description;
     private static ArrayList<Area> areaList = new ArrayList();
     protected Image image;
-    
+
+    Area(String n, String d, String src){
+        name = n;
+        description = d;
+        image = new Image(getClass().getResourceAsStream("../Assets/Images/Area/" + src + ".png"));
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @return the areaList
+     */
+    public static ArrayList<Area> getAreaList() {
+        return areaList;
+    }
+
+    /**
+     * @return the image
+     */
+    public Image getImage() {
+        return image;
+    }
 }
